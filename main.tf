78fe0a2e63304ca728fbf08ab01d48328c2e2cfe
abhishektiwari <tiwarabhishek@google.com>
2021-11-26 16:50:36 +0530

    import_only

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ resource "google_kms_key_ring" "keyring" {
 }
 
 resource "google_kms_crypto_key" "example-key" {
-  name            = "us-dev-appid-encr-test1-cryptokey"
-  key_ring        = google_kms_key_ring.keyring.id
+  name                          = "us-dev-appid-encr-test1-cryptokey"
+  key_ring                      = google_kms_key_ring.keyring.id
   skip_initial_version_creation = true
-  import_only                   = true
-  rotation_period = "7776000s"  # 90 days
+  #import_only                   = true
+  rotation_period = "7776000s" # 90 days
 
   labels = {
     env                  = "dev"
@@ -27,10 +27,10 @@ resource "google_kms_crypto_key" "example-key" {
 }
 
 resource "google_kms_key_ring_import_job" "import-job" {
-  key_ring = google_kms_key_ring.keyring.id
+  key_ring      = google_kms_key_ring.keyring.id
   import_job_id = "my-import-job"
 
-  import_method = "RSA_OAEP_3072_SHA1_AES_256"
+  import_method    = "RSA_OAEP_3072_SHA1_AES_256"
   protection_level = "SOFTWARE"
 }
 
--- version.tf
+++ version.tf
@@ -6,7 +6,7 @@ terraform {
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
       #version = "3.5.0"
       version = "~> 3.87.0"
     }
