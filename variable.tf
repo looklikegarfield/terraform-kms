f481673dc3b0f358dd71fed5343455ce4ab67f74
abhishektiwari <tiwarabhishek@google.com>
2021-12-09 12:12:46 +0530

    new kms

--- variable.tf
+++ variable.tf
@@ -7,16 +7,21 @@ variable "key_path" {
 }
 
 variable "project" {
+  default = "airline1-sabre-wolverine"
 }
 
 variable "keyring_name" {
+  default = "us-dev-appid-encr-test1-keyring"
 }
 
 variable "keyring_location" {
+  default = "us"
 }
 
 variable "keyring_key_name" {
+  default = "us-dev-appid-encr-test1-cryptokey"
 }
 
 variable "keyring_import_job" {
+  default = "my-import-job"
 }
